1217f4e59161e763551b4dd9627c9f3c2b6d4a24
scl <6153394+cphreak@users.noreply.github.com>
2025-08-19 21:13:59 -0700

    CH1L4

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-import os
+import os, sys
 from dotenv import load_dotenv
-
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 from google import genai
-
 client = genai.Client(api_key=api_key)
 
+if len(sys.argv) < 2:
+    print(f"{sys.argv[0]} <prompt>")
+    sys.exit(1)
+else:
+    prompt = sys.argv[1]
 
 
 def main():
     print("Hello from aiagent!")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum?'
+    model='gemini-2.0-flash-001', contents = prompt
     )
     print(response.text)
 
