b7a1bb2fdd181b61560fe6003b311f23b25fcb1b
scl <6153394+cphreak@users.noreply.github.com>
2025-08-20 19:17:46 -0700

    CH1L5

--- main.py
+++ main.py
@@ -4,21 +4,28 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 from google import genai
+from google.genai import types
 client = genai.Client(api_key=api_key)
 
 if len(sys.argv) < 2:
     print(f"{sys.argv[0]} <prompt>")
     sys.exit(1)
 else:
-    prompt = sys.argv[1]
+    user_prompt = sys.argv[1]
 
 
 def main():
     print("Hello from aiagent!")
-    client = genai.Client(api_key=api_key)
+    # response = client.models.generate_content(
+    # model='gemini-2.0-flash-001', contents = user_prompt
+    # )
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        ]
     response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents = prompt
-    )
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        )
     print(response.text)
 
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
