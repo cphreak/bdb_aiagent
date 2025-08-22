098fd07320d0de5b9fcd7d1479788b0cbc06efd1
scl <6153394+cphreak@users.noreply.github.com>
2025-08-21 20:09:27 -0700

    CH1L6

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os, sys
 from dotenv import load_dotenv
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-
+verbose = False
 from google import genai
 from google.genai import types
 client = genai.Client(api_key=api_key)
@@ -12,7 +12,9 @@ if len(sys.argv) < 2:
     sys.exit(1)
 else:
     user_prompt = sys.argv[1]
-
+if len(sys.argv) > 2:
+    if sys.argv[2] == "--verbose":
+        verbose = True
 
 def main():
     print("Hello from aiagent!")
@@ -27,9 +29,10 @@ def main():
         contents=messages,
         )
     print(response.text)
-
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 
